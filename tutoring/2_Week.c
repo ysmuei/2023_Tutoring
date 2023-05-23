1ba43f698c684660e05cae4a6f4b1dd01f9f6180
ysmuei <108169494+ysmuei@users.noreply.github.com>
2023-05-24 00:42:06 +0900

    2주차 추가

--- tutoring/2_Week.c
+++ tutoring/2_Week.c
@@ -12,10 +12,17 @@ void main() {
 	printf("구구단 출력\n");
 	printf("몇단을 출력할까요? : ");
 	scanf_s("%d", &num2);
-	// for 라는 반복문을 사용하여 구구단을 작성. 1부터 9와 같을 때까지 반복하면서
+	// for 라는 반복문을 사용하여 구구단을 작성. i가 1부터 9와 같을 때까지 반복하면서
 	// i가 1씩 증가 
 	for (i = 1; i <= 9; i++) {
 		printf("%d * %d = %d\n", num2, i, num2 * i);
 	}
+	int num3 = 0;
+	printf("\n정수를 입력하세요 : ");
+	scanf_s("%d", &num3);
 
+	printf("10진수 : %d\n", num3); // 서식문자 : %d
+	printf("8진수 : %o\n", num3); // 서식문자 : %o 
+	printf("16진수(소문자) : %x\n", num3); // 서식문자 : %x
+	printf("16진수(대문자) : %X\n", num3); // 서식문자 : %X
 }
